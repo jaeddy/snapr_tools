288f3c4b1aaebc8b18f6b89d22be23a92d4e0dd3
James Eddy <james.a.eddy@gmail.com>
2014-11-18 14:52:15 -0800

    starting code to pull and process files from S3

---
+++ shell/submit_s3_snapr.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Get full list of all BAM files from S3 bucket
+aws s3 ls s3://mayo-prelim-rnaseq/ --recursive | grep .bam | awk '{print $4}' > bam_files.txt
+
+NUM_FILES=$(wc -l bam_files.txt | awk '{print $1}')
+echo $NUM_FILES
+
+for FILE_NUM in $(seq 1 $NUM_FILES); do
+    
+    echo $FILE_NUM
+    FILE=$(awk -v r=$FILE_NUM 'NR==r{print;exit}' bam_files.txt)
+    echo $FILE
+    
+done
+
+# while read line; do
+#     
+#     echo $line ;
+# 
+# done < bam_files.txt
+

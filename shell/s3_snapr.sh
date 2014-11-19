ea516cbb746e8b9e71525b3d4c9b284ec3b58487
James Eddy <james.a.eddy@gmail.com>
2014-11-19 11:29:47 -0800

    script for running snapr on an S3 file

---
+++ shell/s3_snapr.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Define SNAPR reference files
+SNAPR_EXEC=/snapr/bin/snap/snapr
+GENOME_FILE=/mnt/snapr/genome20
+TRANSCRIPTOME_FILE=/mnt/snapr/transcriptome20
+GTF_FILE=/mnt/snapr/Homo_sapiens.GRCh37.68.gtf
+CONTAM_FILE=/mnt/snapr/contamination20
+
+# Define SNAPR settings
+PROCS=8
+# c?
+# d?
+
+
+# Run SNAPR
+time $SNAPR_EXEC paired \
+    $GENOME_FILE \
+    $TRANSCRIPTOME_FILE \
+    $GTF_FILE \
+    $INPUT_FILE_1 \
+    $INPUT_FILE_2 \
+    -o $OUTPUT_FILE \
+    -t $PROCS \
+    -M \
+    -c 2 \
+    -d 4 \
+    -s 0 20000 \
+    -rg $PREFIX \
+    -ct $CONTAM_FILE \
+    -so ;

96bb84c29061ce23b360672577ecc13ab0f3a0bc
James Eddy <james.a.eddy@gmail.com>
2014-11-20 15:46:35 -0800

    trying htseq without samtools

--- shell/s3_htseq.sh
+++ shell/s3_htseq.sh
@@ -37,3 +37,5 @@ SAMTOOLS_EXEC=${ROOT_DIR}bin/samtools-0.1.19/samtools
 HTSEQ_EXEC=${ROOT_DIR}bin/HTSeq-0.6.1/build/scripts-2.7/htseq-count
 
 $SAMTOOLS_EXEC view -h $INPUT_FILE | $HTSEQ_EXEC $GTF_FILE > $OUTPUT_FILE
+# can just input BAM with -f bam option?
+$HTSEQ_EXEC -f bam $INPUT_FILE $GTF_FILE > $OUTPUT_FILE

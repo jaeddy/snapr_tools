ea728a873713a309161c1255eee5bd3004f8a64b
James Eddy <james.a.eddy@gmail.com>
2014-11-20 11:00:52 -0800

    checking that paths are correct

--- shell/s3_snapr.sh
+++ shell/s3_snapr.sh
@@ -2,39 +2,45 @@
 
 S3_PATH=$1
 
+# Parse S3 file path
+S3_DIR=$(dirname $S3_PATH)
 FILE_NAME=$(basename $S3_PATH)
-
 echo $FILE_NAME
 
+# Specify directories
+ROOT_DIR=./
+TMP_DIR=${ROOT_DIR}tmp/
+
+# Create temporary directory for input files
+if [ ! -e "$TMP_DIR" ]; then
+    mkdir "$TMP_DIR"
+fi
+
+# Download S3 file
+INPUT_FILE=${TMP_DIR}${FILE_NAME}
+aws s3 cp \
+    $S3_PATH \
+    $INPUT_FILE ;
+
+# Define SNAPR output file
+PREFIX=${$FILE_NAME%.bam}
+OUTPUT_FILE=${TMP_DIR}${PREFIX}.snap.bam
+
 # Define SNAPR reference files
-# SNAPR_EXEC=/snapr/bin/snap/snapr
-# GENOME_FILE=/mnt/snapr/genome20
-# TRANSCRIPTOME_FILE=/mnt/snapr/transcriptome20
-# GTF_FILE=/mnt/snapr/Homo_sapiens.GRCh37.68.gtf
-# CONTAM_FILE=/mnt/snapr/contamination20
+SNAPR_EXEC=/bin/snap/snapr
+GENOME_FILE=/mnt/genome20
+TRANSCRIPTOME_FILE=/mnt/transcriptome20
+GTF_FILE=/mnt/Homo_sapiens.GRCh37.68.gtf
+CONTAM_FILE=/mnt/contamination20
 
-# Define SNAPR settings
-# PROCS=8
-# c?
-# d?
-
-# Define input files
-# INPUT_FILE_1=
-# INPUT_FILE_2=
 
 # Run SNAPR
 # time $SNAPR_EXEC paired \
 #     $GENOME_FILE \
 #     $TRANSCRIPTOME_FILE \
 #     $GTF_FILE \
-#     $INPUT_FILE_1 \
-#     $INPUT_FILE_2 \
+#     $INPUT_FILE \
 #     -o $OUTPUT_FILE \
-#     -t $PROCS \
 #     -M \
-#     -c 2 \
-#     -d 4 \
-#     -s 0 20000 \
 #     -rg $PREFIX \
-#     -ct $CONTAM_FILE \
 #     -so ;

cc317cc98c18567b13aaf71789f457594f47c2c7
James Eddy <james.a.eddy@gmail.com>
2014-11-21 14:11:12 -0800

    modifying submit script to work for specified groups

--- shell/submit_s3_snapr.sh
+++ shell/submit_s3_snapr.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# Get full list of all BAM files from S3 bucket
-aws s3 ls s3://mayo-prelim-rnaseq/ --recursive | grep .bam$ | awk '{print $4}' > bam_files.txt
+BUCKET=$1
+GROUP=$2
+
+# Get full list of all BAM files from S3 bucket for the specified group
+aws s3 ls ${BUCKET}/${GROUP} --recursive | grep .bam$ | awk '{print $4}' > ${GROUP}_bam_files.txt
 
 NUM_FILES=1
 #NUM_FILES=$(wc -l bam_files.txt | awk '{print $1}')
@@ -10,9 +13,9 @@ NUM_FILES=1
 for FILE_NUM in $(seq 1 $NUM_FILES); do
     
 #    echo $FILE_NUM
-    FILE=$(awk -v r=$FILE_NUM 'NR==r{print;exit}' bam_files.txt)
+    FILE=$(awk -v r=$FILE_NUM 'NR==r{print;exit}' ${GROUP}_bam_files.txt)
     echo $FILE
-    S3_PATH=s3://mayo-prelim-rnaseq/$FILE
+    S3_PATH=$BUCKET/$GROUP/$FILE
     echo $S3_PATH
     
 done

2bd9515a6c37fe06aeb8419712696e526ad4c303
root <james.a.eddy@gmail.com>
2014-11-22 23:08:26 +0000

    modifying path for SGE stdout and stderr logs

--- shell/submit_s3_snapr.sh
+++ shell/submit_s3_snapr.sh
@@ -11,6 +11,15 @@ NUM_FILES=1
 #NUM_FILES=$(wc -l bam_files.txt | awk '{print $1}')
 #echo $NUM_FILES
 
+# Set up directory and labels for logging
+SNAPR_VOL=/mnt/
+LOG_DIR=${SNAPR_VOL}sge_logs/
+if [ ! -e "$LOG_DIR" ]; then
+    mkdir "$LOG_DIR"
+fi
+TAG=_snapr
+
+# Specify path to job script
 SCRIPT_PATH=/${EBS_NAME}/snapr_tools/shell/s3_snapr.sh
 
 for FILE_NUM in $(seq 1 $NUM_FILES); do
@@ -18,8 +27,11 @@ for FILE_NUM in $(seq 1 $NUM_FILES); do
 #    echo $FILE_NUM
     FILE=$(awk -v r=$FILE_NUM 'NR==r{print;exit}' ${GROUP}_bam_files.txt)
     S3_PATH=$BUCKET/$FILE
+    JOBNAME=$(basename $S3_PATH)
 
     qsub -V -pe orte 16 \
+        -o ${LOG_DIR}${JOB_NAME}${TAG}.o \
+        -e ${LOG_DIR}${JOB_NAME}${TAG}.e \
         -b y $SCRIPT_PATH $S3_PATH $EBS_NAME ;
     
 done

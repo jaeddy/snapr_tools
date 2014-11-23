2988c325a3540920f832c0f059f5c9c5f1fc4a65
root <james.a.eddy@gmail.com>
2014-11-23 07:56:13 +0000

    submit script updated to run on full set of files

--- shell/submit_s3_snapr.sh
+++ shell/submit_s3_snapr.sh
@@ -7,8 +7,8 @@ EBS_NAME=$3
 # Get full list of all BAM files from S3 bucket for the specified group
 aws s3 ls ${BUCKET}/${GROUP} --recursive | grep .bam$ | awk '{print $4}' > ${GROUP}_bam_files.txt
 
-NUM_FILES=2
-#NUM_FILES=$(wc -l bam_files.txt | awk '{print $1}')
+#NUM_FILES=2
+NUM_FILES=$(wc -l bam_files.txt | awk '{print $1}')
 #echo $NUM_FILES
 
 # Specify path to job script

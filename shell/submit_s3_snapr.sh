beaaa047ae0f3297606a7d08492b4393f7f11073
root <james.a.eddy@gmail.com>
2014-11-24 18:03:21 +0000

    modifying submit script to work on specific subdirectory

--- shell/submit_s3_snapr.sh
+++ shell/submit_s3_snapr.sh
@@ -7,8 +7,8 @@ EBS_NAME=$3
 # Get full list of all BAM files from S3 bucket for the specified group
 aws s3 ls ${BUCKET}/${GROUP} --recursive | grep .bam$ | awk '{print $4}' > ${GROUP}_bam_files.txt
 
-#NUM_FILES=2
-NUM_FILES=$(wc -l bam_files.txt | awk '{print $1}')
+NUM_FILES=2
+#NUM_FILES=$(wc -l bam_files.txt | awk '{print $1}')
 #echo $NUM_FILES
 
 # Specify path to job script
@@ -18,13 +18,15 @@ for FILE_NUM in $(seq 1 $NUM_FILES); do
     
 #    echo $FILE_NUM
     FILE=$(awk -v r=$FILE_NUM 'NR==r{print;exit}' ${GROUP}_bam_files.txt)
-    S3_PATH=$BUCKET/$FILE
+    S3_PATH=$(dirname $BUCKET)/$FILE
+    echo $S3_PATH
     JOB_NAME=$(basename $S3_PATH)
+    echo $JOB_NAME
 #    echo $JOB_NAME
-    qsub -V -pe orte 16 \
-        -o ${JOB_NAME}${TAG}.o \
-        -e ${JOB_NAME}${TAG}.e \
-        -b y $SCRIPT_PATH $S3_PATH $EBS_NAME ;
+#    qsub -V -pe orte 16 \
+#        -o ${JOB_NAME}${TAG}.o \
+#        -e ${JOB_NAME}${TAG}.e \
+#        -b y $SCRIPT_PATH $S3_PATH $EBS_NAME ;
     
 done
 

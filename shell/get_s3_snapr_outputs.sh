d34c0e1c1b6bef59b826b81eb3351c8fd08cb938
James Eddy <james.a.eddy@gmail.com>
2014-11-26 12:40:09 -0800

    updating script to download snapr results

--- shell/get_s3_snapr_outputs.sh
+++ shell/get_s3_snapr_outputs.sh
@@ -15,19 +15,17 @@ if [ ! -e $RESULTS_DIR ]; then
     mkdir $RESULTS_DIR
 fi
 
-NUM_FILES=1
-#NUM_FILES=$(wc -l snapr_output_files.txt | awk '{print $1}')
-#echo $NUM_FILES
+NUM_FILES=$(wc -l snapr_output_files.txt | awk '{print $1}')
+echo "$NUM_FILES files detected..."
+# NUM_FILES=1
 
 for FILE_NUM in $(seq 1 $NUM_FILES); do
     
-#    echo $FILE_NUM
+    echo "Copying file $FILE_NUM of $NUM_FILES..."
     FILE=$(awk -v r=$FILE_NUM 'NR==r{print;exit}' snapr_output_files.txt)
     S3_PATH=$BUCKET/$FILE
-    echo $S3_PATH
     
     LOCAL_PATH=$RESULTS_DIR/$FILE
-    echo $LOCAL_PATH
     
     aws s3 cp $S3_PATH $LOCAL_PATH
 done

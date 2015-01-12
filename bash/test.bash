9784f3e4f32d26ad7921ca9d45de1cb3429d4fdd
James Eddy <james.a.eddy@gmail.com>
2015-01-12 13:22:40 -0800

    testing changes for file format options

--- bash/test.bash
+++ bash/test.bash
@@ -1,45 +1,75 @@
 #!/bin/bash
 
-PROCS=16
-MEM="230.0G"
-QUEUE=all.q
-GENONLY=0
-JOBNAME="snap-rna"
-EMAIL="bob@bob.com"
-NODE="node001"
+# BUCKET="s3://mayo-prelim-rnaseq"
+# SUBDIR="AD_Samples"
+
+
+BUCKET="s3://ufl-u01-rnaseq"
+
+
+NUM=16
+STR="230.0G"
+NAME='default'
 FILE1='file1.fastq'
 FILE2='file2.fastq'
-PREFIX='default'
-OUTPUT='/results'
-SAVE_BAM=0
-
-function usage {
-	echo "$0: [-m mem(3.8G,15.8G) [-p num_slots] [-q queue] [-N jobname] [-e email_address] [-g] -p prefix -1 file1.fastq -2 file2.fastq -o output_dir -b"
-	echo
-}
-
-while getopts "p:n:q:m:N:e:o:1:2:gh" ARG; do
+FORMAT=fastq
+# 
+# function usage {
+# 	echo "$0: [-m mem(3.8G,15.8G) [-p num_slots] [-q queue] [-N jobname] [-e email_address] [-g] -p prefix -1 file1.fastq -2 file2.fastq -o output_dir -b"
+# 	echo
+# }
+# 
+while getopts "b:n:s:N:1:2:f:h" ARG; do
 	case "$ARG" in
-		q ) QUEUE=$OPTARG;;
-		m ) MEM=$OPTARG;;
-		N ) JOBNAME=$OPTARG;;
-		e ) EMAIL=$OPTARG;;
-		g ) GENONLY=1;;
-    	r ) OPTIONS=$OPTARG;;
+	    b ) BUCKET=$OPTARG;;
+		n ) NUM=$OPTARG;;
+		s ) STR=$OPTARG;;
+		N ) NAME=$OPTARG;;
         1 ) FILE1=$OPTARG;;
         2 ) FILE2=$OPTARG;;
-        p ) PREFIX=$OPTARG;;
-        o ) OUTPUT=$OPTARG;;
-        b ) SAVE_BAM=1;;
+        f ) FORMAT=$OPTARG;;
 		h ) usage; exit 0;;
 		* ) usage; exit 1;;
 	esac
 done
 shift $(($OPTIND - 1)) 
 
-APP="/snap/bin/snap/snap-rna"
-OPTIONS="paired /snap/Genomes/GRCh37/snap/genome20/ /snap/Genomes/GRCh37/snap/transcriptome20/ /snap/Genomes/GRCh37/gtf/Homo_sapiens.GRCh37.68.gtf ${FILE1} ${FILE2} -o ${PREFIX}.snap.bam -t $PROCS -M -c 2 -d 4 -s 0 20000 -so -rg $PREFIX"
 
-echo $OPTIONS
+echo $FORMAT
 
-GENOME="/snap/Genomes/GRCh37/fasta/Homo_sapiens.GRCh37.68.genome.fa"
+case "$FORMAT" in
+    bam ) EXTENSION=.bam;;
+    fastq ) EXTENSION=.fastq.gz;;
+esac
+
+echo $EXTENSION
+
+# Get full list of all BAM files from S3 bucket for the specified group
+FILE_LIST=`mktemp s3-seq-files.XXXXXXXX`
+
+aws s3 ls ${BUCKET}/${SUBDIR} --recursive \
+    | grep ${EXTENSION}$ \
+    | awk '{print $4}' \
+    > $FILE_LIST ;
+
+NUM_FILES=$(wc -l ${FILE_LIST} | awk '{print $1}')
+echo "$NUM_FILES files..."
+
+function get_id {
+    while read line 
+    do
+        filename=${line##*/};
+        fileid=${filename%%.*}
+        echo $fileid;
+    done < $1
+}
+
+# Get list of unique sample identifiers
+ID_LIST=`mktemp s3-seq-ids.XXXXXXXX`
+
+get_id ${FILE_LIST} | uniq > $ID_LIST
+
+NUM_IDS=$(wc -l ${ID_LIST} | awk '{print $1}')
+echo "$NUM_IDS ids..."
+
+# rm $FILE_LIST

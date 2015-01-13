7ec545e9bd8cf4d639f58207fd3c3ca45b5ba71e
James Eddy <james.a.eddy@gmail.com>
2015-01-13 15:57:32 -0800

    testing input options/arguments

--- bash/s3_snapr.bash
+++ bash/s3_snapr.bash
@@ -3,76 +3,78 @@
 S3_PATH=$1
 
 # Define SNAPR reference files
-ASSEMBLY_NAME=Homo_sapiens.GRCh38
-ASSEMBLY_VER=.77
 SNAPR_EXEC=${ROOT_DIR}bin/snapr/snapr
 
-GENOME=${SNAPR_VOL}genome20
-TRANSCRIPTOME_DIR=${SNAPR_VOL}transcriptome20
-GTF_FILE=${SNAPR_VOL}${ASSEMBLY_NAME}${ASSEMBLY_VER}.gtf
+GENOME=/mnt/genome20
+TRANSCRIPTOME_DIR=/mnt/transcriptome20
+ENSEMBL=/mnt/Homo_sapiens.GRCh38.77.gtf
 
-# Parse S3 file path
-S3_DIR=$(dirname $S3_PATH)
-FILE_NAME=$(basename $S3_PATH)
-PREFIX=${FILE_NAME%.bam}
-echo $PREFIX
+MODE=single
+
 
 # Specify directories
 ROOT_DIR=/$2/
 SNAPR_VOL=/mnt/
 TMP_DIR=${SNAPR_VOL}${PREFIX}_tmp/
 
-while getopts "b:n:s:N:1:2:f:h" ARG; do
+while getopts "p1:2:g:t:e:h" ARG; do
 	case "$ARG" in
-	    b ) BUCKET=$OPTARG;;
-		n ) PROCS=$OPTARG;;
-		s ) MEM=$OPTARG;;
-		N ) NAME=$OPTARG;;
-        1 ) FILE1=$OPTARG;;
-        2 ) FILE2=$OPTARG;;
-        f ) FORMAT=$OPTARG;;
+	    p ) MODE=paired;;
+	    1 ) PATH1=$OPTARG;;
+	    2 ) PATH2=$OPTARG;;
+		g ) GENOME=$OPTARG;;
+		t ) TRANSCRIPTOME=$OPTARG;;
+		e ) ENSEMBL=$OPTARG;;
 		h ) usage; exit 0;;
 		* ) usage; exit 1;;
 	esac
 done
-shift $(($OPTIND - 1)) 
+shift $(($OPTIND - 1))
 
+echo "$MODE $GENOME $PATH1"
+
+# Parse S3 file path
+# S3_DIR=$(dirname $S3_PATH)
+# FILE_NAME=$(basename $S3_PATH)
+# PREFIX=${FILE_NAME%.bam}
+# echo $PREFIX
+# 
 # Create temporary directory for input files
-if [ ! -e "$TMP_DIR" ]; then
-    mkdir "$TMP_DIR"
-fi
-
+# if [ ! -e "$TMP_DIR" ]; then
+#     mkdir "$TMP_DIR"
+# fi
+# 
 # Download S3 file
-INPUT_FILE=${TMP_DIR}${FILE_NAME}
-echo "Copying $S3_PATH to $INPUT_FILE"
-
-aws s3 cp \
-    $S3_PATH \
-    $INPUT_FILE ;
-
+# INPUT_FILE=${TMP_DIR}${FILE_NAME}
+# echo "Copying $S3_PATH to $INPUT_FILE"
+# 
+# aws s3 cp \
+#     $S3_PATH \
+#     $INPUT_FILE ;
+# 
 # Define SNAPR output file
-OUTPUT_FILE=${TMP_DIR}${PREFIX}.snap.bam
-
+# OUTPUT_FILE=${TMP_DIR}${PREFIX}.snap.bam
+# 
 # Run SNAPR
-time $SNAPR_EXEC paired \
-    $GENOME \
-    $TRANSCRIPTOME \
-    $ENSEMBLE \
-    $INPUT_FILE \
-    -o $OUTPUT_FILE \
-    -M \
-    -rg $PREFIX \
-    -so \
-    -ku ;
-
+# time $SNAPR_EXEC $MODE \
+#     $GENOME \
+#     $TRANSCRIPTOME \
+#     $ENSEMBLE \
+#     $INPUT_FILE \
+#     -o $OUTPUT_FILE \
+#     -M \
+#     -rg $PREFIX \
+#     -so \
+#     -ku ;
+# 
 # Remove original file
-rm $INPUT_FILE
-
+# rm $INPUT_FILE
+# 
 # Copy files to S3
-aws s3 cp \
-    $TMP_DIR \
-    $S3_DIR/snapr/ \
-    --recursive ;
-
+# aws s3 cp \
+#     $TMP_DIR \
+#     $S3_DIR/snapr/ \
+#     --recursive ;
+# 
 # Remove temporary directory
-rm -rf $TMP_DIR
+# rm -rf $TMP_DIR

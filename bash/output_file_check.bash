3e6774857941b12571f1ab0110d27e4aa845a53d
James Eddy <james.a.eddy@gmail.com>
2015-01-20 16:09:51 -0800

    script to check for any missing output files

---
+++ bash/output_file_check.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+BUCKET="s3://mayo-prelim-rnaseq"
+SUBDIR="PSP_Samples"
+EXTENSION=.bam
+
+# Get full list of all files from S3 bucket for the specified group
+FILE_LIST=`mktemp s3-seq-files.XXXXXXXX`
+aws s3 ls ${BUCKET}/${SUBDIR} --recursive \
+    | grep -e "${EXTENSION}$" -e ".bam$" \
+    | awk '{print $4}' \
+    > $FILE_LIST ;
+
+# Pull out list of source files
+SOURCE_FILES=`mktemp s3-source-files.XXXXXXXX`
+grep -v .snap $FILE_LIST > $SOURCE_FILES
+echo $(wc -l < $SOURCE_FILES)
+
+# Pull out list of output SNAPR files
+SNAPR_FILES=`mktemp s3-snapr-files.XXXXXXXX`
+grep .snap $FILE_LIST > $SNAPR_FILES
+echo $(wc -l < $SNAPR_FILES)
+
+# Function to strip filenames, leaving only sample ID
+function get_id {
+    while read line 
+    do
+        filename=${line##*/};
+        fileid=${filename%%.*}
+        echo $fileid;
+    done < $1
+} 
+
+MISSED_IDS=`mktemp missed-files.XXXXXXXX`
+comm -23 <(get_id $SOURCE_FILES) <(get_id $SNAPR_FILES) > $MISSED_IDS
+
+grep -f $MISSED_IDS $SOURCE_FILES
+
+rm $FILE_LIST
+rm $SOURCE_FILES
+rm $SNAPR_FILES
+rm $MISSED_IDS
